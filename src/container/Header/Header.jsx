bbdd00ed093e2cad1f44040239854755a725efc6
DaviD D <jdavid.martinez@gmail.com>
2022-03-03 20:22:42 -0500

    I try to make a deployer version-3

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,8 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Escoge nuestro nueva hamburguesa" />
-      <h1 className="app__header-h1">Hamburguesa artesanal</h1>
+      <SubHeading title="Escoge nuestra nueva hamburguesa" />
+      <h1 className="app__header-h1">Hamburguesa Nostreña</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Nuestra Hamburguesa Norteña, combina el gusto mexicano con los secretos de la comida Colombiana, para brindarte equilibrio en textura y sabor </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
