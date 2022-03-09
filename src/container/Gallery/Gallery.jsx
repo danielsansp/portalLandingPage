fc79f04b14e1f44f73e1eeeb645b2f4b464ca9d6
DaviD D <jdavid.martinez@gmail.com>
2022-03-09 14:56:56 -0500

    Add <a> tag to link Instagram ount to Instagram icon in crousel

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
-import { SubHeading } from '../../components';
+// import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
@@ -21,17 +21,19 @@ const Gallery = () => {
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
-        <SubHeading title="Instagram" />
+        {/* <SubHeading title="Instagram" /> */}
         <h1 className="headtext__cormorant">Hamburguesas</h1>
-        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Estos son nuestros deliciosas Hamburguesas artesanales</p>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Estas son nuestras deliciosas Hamburguesas artesanales</p>
         {/* <button type="button" className="custom__button">View More</button> */}
       </div>
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-          {[images.rancheraBurger, images.rancheraBurger, images.rancheraBurger, images.rancheraBurger, images.portazoBrancheraBurgerurger].map((image, index) => (
+          {[images.rancheraBurger, images.rancheraBurger, images.rancheraBurger, images.rancheraBurger, images.rancheraBurger].map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <img src={image} alt="gallery_image" />
-              <BsInstagram className="gallery__image-icon" />
+              <a href="https://instagram.com/portal.st50?utm_medium=copy_link" target="_blank" rel="noreferrer noopener" className="Instagram_link">
+                <BsInstagram className="gallery__image-icon" />
+              </a>
             </div>
           ))}
         </div>
