7e66e5f88de519bffe9335c7dd75c1636eacf3c7
DaviD D <jdavid.martinez@gmail.com>
2022-03-12 16:13:09 -0500

    working on a new carousel, as it is not usefull yet, so I do not push it yet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Footer, Gallery, Header, SandwichGallery, PerrosGallery } from './container';
-import { Navbar } from './components';
+import { Footer, Gallery, Header, SandwichGallery, PerrosGallery} from './container';
+import { Navbar} from './components';
 import './App.css';
 
 const App = () => (
@@ -11,6 +11,7 @@ const App = () => (
     <Gallery />
     <SandwichGallery />
     <PerrosGallery />
+    {/* <Carousel /> */}
     <Footer />
   </div>
 );
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
 
 import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 import { images } from '../../constants';
 
 import './Carousel.css';
 
-const Carousel = () => (
+const Carousel = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
   <div>
     <h2> Single Item</h2>
-    <Slider dots="false" infinite="true" speed="500" slidesToShow="1" slidesToScroll="1">
+    <Slider {...settings} >
       <div>
-        <h3>3</h3>
+        <img src={images.rancheraBurger} alt="Hamburguesa Portal" />
       </div>
       <div>
-        {images.rancheraBurger}
+        <img src={images.rancheraBurger} alt="Hamburguesa Portal" />
       </div>
       <div>
-        {images.rancheraBurger}
+        <img src={images.rancheraBurger} alt="Hamburguesa Portal" />
       </div>
       <div>
-        {images.rancheraBurger}
+        <img src={images.rancheraBurger} alt="Hamburguesa Portal" />
       </div>
       <div>
-        {images.rancheraBurger}
-      </div>
-      <div>
-        {images.rancheraBurger}
+        <img src={images.rancheraBurger} alt="Hamburguesa Portal" />
       </div>
     </Slider>
   </div>
-);
+)};
 export default Carousel;
 
 /* const settings = {
