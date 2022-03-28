e65244dbceb94f3756672ac13b32bf5b248d9967
DaviD D <jdavid.martinez@gmail.com>
2022-03-28 16:59:40 -0500

    modify Navbar component

--- src/components/Navbar/Navbar copy.jsx
+++ src/components/Navbar/Navbar copy.jsx
@@ -5,7 +5,7 @@ import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
-  // const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -18,27 +18,26 @@ const Navbar = () => {
         <li className="p__opensans"><a href="#awards">Consejos</a></li>
         <li className="p__opensans"><a href="#contact">Contactanos</a></li>
       </ul>
-      
-      {/* { /* <div className="app__navbar-login">
+      { /* <div className="app__navbar-login">
         <a href="#login" className="p__opensans">Log In / Registration</a>
         <div />
         <a href="/" className="p__opensans">Book Table</a>
       </div>  */ }
-      {/* <div className="app__navbar-smallscreen"> */}
-        {/* <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} /> */}
-        {/* {toggleMenu && ( */}
-          {/* <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={"/"} />
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
-              <li><a href="#home" onClick={"/"}>Home</a></li>
-              <li><a href="#menu" onClick={"/"}>Menu</a></li>
-              <li><a href="#about" onClick={"/"}>Nosotros</a></li>
-              <li><a href="#awards" onClick={"/"}>Consejos</a></li>
-              <li><a href="#contact" onClick={"/"}>Contactanos</a></li>
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>Nosotros</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Consejos</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contactanos</a></li>
             </ul>
-          </div> */}
-        {/* )} */}
-      {/* </div> */}
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
