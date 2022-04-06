fc41dc208071a648e6d87dd32f0d394810829e5c
DaviD D <jdavid.martinez@gmail.com>
2022-04-06 15:36:05 -0500

    Remove suheading section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { SubHeading, WhatsAppButton } from '../../components';
+import { WhatsAppButton } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Escoge nuestra nueva hamburguesa" />
+      {/* <SubHeading title="Escoge nuestra nueva hamburguesa" /> */}
       <h1 className="app__header-h1">Hamburguesa Norteña</h1>
       <p className="p__opensans">Nuestra Hamburguesa Norteña, combina el gusto mexicano con los secretos de la comida Colombiana, para brindarte equilibrio en textura y sabor </p>
     </div>
